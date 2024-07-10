8724ee3557d58d9910e1d1848283f5c71209ef9c
TovarischPootis <54110479+TovarischPootis@users.noreply.github.com>
2024-07-10 17:49:48 +0200

    Search bar

--- tum_meteo_tut/urls.py
+++ tum_meteo_tut/urls.py
@@ -4,5 +4,7 @@ from tum_meteo_tut import views
 
 urlpatterns = [
     path("tum_meteo_tut/", views.temp_here, name="temp_here"),
-    path("tum_meteo_tut/discover", views.temp_somewhere, name="temp_somewhere")
+    path("tum_meteo_tut/discover", views.temp_somewhere, name="temp_somewhere"),
+    path("tum_meteo_tut/search", views.search, name="search"),
+    path("tum_meteo_tut/search_internal", views.find_city, name="find_city")
 ]
--- tum_meteo_tut/views.py
+++ tum_meteo_tut/views.py
@@ -1,36 +1,71 @@
 from datetime import datetime
 
+import json
 import geocoder as geocoder
 import requests
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from tum_meteo_tut.models import Worldcities
 
 
-# Create your views here.
-def temp_here(request):
-    location = geocoder.ip('me').latlng
-    temp = get_temp(location)
+# all_cities = [cityData.city for cityData in Worldcities.objects.all()]
+
+# print(all_cities)
+
+def find_city(request):
+    city = request.GET.get('city')
+    payload = []
+    if city:
+        city_objs = Worldcities.objects.filter(city__icontains=city)
+        payload = [cityData.city for cityData in city_objs]
+
+    return JsonResponse({'status': 200, 'data': payload})
+
+
+def display_info(request, city, city_temp, country, lat, lng):
     template = loader.get_template('index.html')
     context = {
-        'city': 'Your Location',
-        'temp': temp
+        'city': city,
+        'temp': city_temp,
+        'country': country,
+        'lat': lat,
+        'lng': lng
     }
+
     return HttpResponse(template.render(context, request))
+
+
+def search(request):
+    cityData = Worldcities.objects.filter(city__icontains=request.GET.get('city')).first()
+    city = cityData.city
+    location = [cityData.lat, cityData.lng]
+    temp = get_temp(location)
+    country = cityData.country
+
+    return display_info(request, city, temp, country, cityData.lat, cityData.lng)
+
+
+def temp_here(request):
+    loc_info = geocoder.ipinfo('me')
+    loc = loc_info.latlng
+    temp = get_temp(loc)
+    city = loc_info.city
+    country_code = loc_info.country
+
+    with open("tum_meteo_tut/country_codes.json") as json_file:
+        json_data = json.load(json_file)
+        country = json_data[country_code] or country_code
+
+    return display_info(request, f"Your Location ({city})", temp, country, loc_info.lat, loc_info.lng)
 
 
 def temp_somewhere(request):
     random_item = Worldcities.objects.all().order_by('?').first()
     city = random_item.city
+    country = random_item.country
     location = [random_item.lat, random_item.lng]
     temp = get_temp(location)
-    template = loader.get_template('index.html')
-    context = {
-        'city': city,
-        'temp': temp
-    }
-
-    return HttpResponse(template.render(context, request))
+    return display_info(request, city, temp, country, random_item.lat, random_item.lng)
 
 
 def get_temp(location):
